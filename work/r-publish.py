64df22c5af1682473ce4c5c40c903a2ad0fd7c3b
pangguoping <work2312@163.com>
2016-07-30 07:48:41 +0800

    add rabbitmq

--- work/r-publish.py
+++ work/r-publish.py
@@ -4,11 +4,13 @@
 
 import pika
 credentials = pika.PlainCredentials('admin', 'admin')
-connection = pika.BlockingConnection(pika.ConnectionParameters('dev-api-node2',56720,'/',credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters('dev-api-node2',5672,'/',credentials))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello', durable=True)  #队列持久化
+channel.queue_declare(queue='hello2', durable=True)  #队列持久化
 #channel.queue_declare(queue='hello')
-channel.basic_publish(exchange='',routing_key='hello',body='Hello World!',properties=pika.BasicProperties(delivery_mode = 2,))
+channel.basic_publish(exchange='',routing_key='hello2',body='Hello World!',properties=pika.BasicProperties(delivery_mode = 2,))
+#channel.basic_publish(exchange='',routing_key='hello',body='Hello World!')
+
 print("[x] Sent 'Hello World'")
 connection.close()
